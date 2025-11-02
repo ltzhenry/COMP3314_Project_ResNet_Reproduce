846ab567f18d8a89dd961ecafde349a132196866
IvanChen <yifanChen041226@hotmail.com>
2025-11-02 17:24:13 +0800

    Add dataset loader

--- datasets/cifar10_loader.py
+++ datasets/cifar10_loader.py
@@ -14,3 +14,53 @@ TODO:
     - Normalize to [-1, 1] range per channel
     - Return training and testing dataloaders
 """
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_cifar10_loaders(root="./data", batch_size=128, num_workers=4):
+    """
+    Loads CIFAR-10 dataset with the standard ResNet-style preprocessing.
+
+    Args:
+        root (str): Path to store/load the dataset.
+        batch_size (int): Batch size for both train and test.
+        num_workers (int): Number of subprocesses for data loading.
+
+    Returns:
+        train_loader (DataLoader)
+        test_loader (DataLoader)
+    """
+    cifar10_mean = (0.5, 0.5, 0.5)
+    cifar10_std  = (0.5, 0.5, 0.5)
+
+    # --- Data augmentation for training set ---
+    train_transform = transforms.Compose([
+        transforms.Pad(4),                          # 4-pixel padding
+        transforms.RandomCrop(32),                  # Random 32x32 crop
+        transforms.RandomHorizontalFlip(),          # Random flip
+        transforms.ToTensor(),                      # Convert to [0,1] tensor
+        transforms.Normalize(cifar10_mean, cifar10_std),  # Normalize to [-1,1]
+    ])
+
+    # --- Testing set: only normalize ---
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(cifar10_mean, cifar10_std),
+    ])
+
+    # --- Dataset loading ---
+    train_dataset = datasets.CIFAR10(root=root, train=True, download=True,
+                                     transform=train_transform)
+    test_dataset  = datasets.CIFAR10(root=root, train=False, download=True,
+                                     transform=test_transform)
+
+    # --- Data loaders ---
+    train_loader = DataLoader(train_dataset, batch_size=batch_size,
+                              shuffle=True, num_workers=num_workers,
+                              pin_memory=True)
+    test_loader  = DataLoader(test_dataset, batch_size=batch_size,
+                              shuffle=False, num_workers=num_workers,
+                              pin_memory=True)
+
+    return train_loader, test_loader
